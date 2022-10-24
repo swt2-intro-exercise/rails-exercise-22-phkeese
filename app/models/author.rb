386efefb87060dd479010da566ffeb696089ddae
Philipp Keese <philipp.keese@student.hpi.de>
2022-10-24 12:10:23 +0200

    fix #3

---
+++ app/models/author.rb
@@ -0,0 +1,7 @@
+class Author < ApplicationRecord
+	# def initialize(first_name, last_name, homepage)
+	# 	@first_name = first_name
+	# 	@last_name = last_name
+	# 	@homepage = homepage
+	# end
+end
---
+++ spec/models/author_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  it "should have a first name" do
+    author = Author.new(:first_name => "Alan")
+    expect(author.first_name).to eq("Alan")
+  end
+  
+  it "should have a last name" do
+    author = Author.new(:last_name => "Turing")
+    expect(author.last_name).to eq("Turing")
+  end
+  
+  it "should have a homepage" do
+    author = Author.new(:homepage => "http://wikipedia.org/Alan_Turing")
+    expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+  end
+
+end
