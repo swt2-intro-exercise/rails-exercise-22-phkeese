0459cd88b36ef46305eb5e06c68feb4f838402fb
Philipp Keese <philipp.keese@student.hpi.de>
2022-11-11 18:41:58 +0100

    try to fix mess from before

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,8 @@
 class Author < ApplicationRecord
+	has_and_belongs_to_many :papers
 	def name
 		return "#{self.first_name} #{self.last_name}"
 	end
 
 	validates :last_name, presence: true
-
-	def papers
-		[]
-	end
 end
