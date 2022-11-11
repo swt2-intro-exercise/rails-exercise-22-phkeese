31732dd409e980d7ad7d8a89a31fee9dea585b83
Philipp Keese <philipp.keese@student.hpi.de>
2022-11-11 17:16:49 +0100

    tests for #12

--- spec/features/author/delete_author_spec.rb
+++ spec/features/author/delete_author_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 describe "Delete author action", type: :feature do
-  it "should delete an author" do
+  before(:each) do
     @author = Author.new(:first_name => "Alan", :last_name => "Turing")
     @author.save
+  end
 
+  it "should delete an author" do
     visit authors_path
 
-    expect(page).to have_link("Delete", :href => delete)
+    expect(Author.find(@author.id)).to_not be_nil
+    click_on "Delete"
+    expect { Author.find(@author.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it "should decrease the author count" do
+    visit authors_path
+
+    count = Author.count
+    click_on "Delete"
+    expect(Author.count).to be < count
+  end
+
+
 end
