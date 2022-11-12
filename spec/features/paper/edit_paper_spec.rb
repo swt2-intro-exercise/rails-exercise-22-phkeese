3eb7aacf001acfcd29aa76767d64fb1d1f6da3b5
Philipp Keese <philipp.keese@student.hpi.de>
2022-11-12 23:38:52 +0100

    add authors to papers

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -2,20 +2,17 @@ require 'rails_helper'
 
 describe "Edit paper page", type: :feature do
   before(:each) do
-    @paper = Paper.new(
-      title: "My Paper",
-      venue: "HPI",
-      year: 2022,
-    )
-    @paper.save
+    @paper = FactoryBot.create :paper
+    @author = FactoryBot.create :author, first_name: "Aberforth"
   end
     it "should have inputs for the paper's title, venue and year" do
-      visit edit_paper_path(@paper.id
-            )
+      visit edit_paper_path(@paper.id)
+
       # these are the standard names given to inputs by the Rails form builder
       expect(page).to have_field('paper[title]')
       expect(page).to have_field('paper[venue]')
       expect(page).to have_field('paper[year]')
+      expect(page).to have_field('paper[author_ids][]')
   end
 
   it "should update a paper entry on submit" do
@@ -26,5 +23,16 @@ describe "Edit paper page", type: :feature do
     @paper.reload
 
     expect(@paper.title).to eq("GPU Gems")
+  end
+
+  it "should have a multiselect for authors" do
+    visit edit_paper_path(@paper.id)
+
+    select @author.name, from: "paper[author_ids][]"
+
+    find('input[type="submit"]').click
+    @paper.reload
+
+    expect(@paper.authors).to include(@author)
   end
 end
